f47e938e1a0eab95958a7f0d9a2a24ed4f2eea1f
Behruz <ergashevbehruz.uz@gmail.com>
2025-01-22 16:00:37 +0500

    v4.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import "./App.css";
 import Header from "./layout/Header/Header.jsx";
 import Footer from "./layout/Footer/Footer.jsx";
-import "./assets/fonts/fonts.css";
+import "/public/fonts/fonts.css";
 import { BrowserRouter as Router, Route, Routes, Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 const App = () => {
   const navigate = useNavigate();
